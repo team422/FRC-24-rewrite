93787504802efc2ace67fb2dac204752291c0067
keckothedragon <flappystar05@gmail.com>
2024-05-09 20:50:23 -0400

    Code cleanup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,6 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the button bindings
     configureSubsystems();
     configureControllers();
     configureButtonBindings();
